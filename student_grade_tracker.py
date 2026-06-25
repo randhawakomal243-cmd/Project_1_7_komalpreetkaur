651c4cbb47ef4da1a33fe2664756559e4d11bffe
Komalpreet Kaur <randhawakomal243@gmail.com>
2026-06-25 11:38:14 -0400

    Add full working student grade tracker program

---
+++ student_grade_tracker.py
@@ -0,0 +1,54 @@
+"""
+Program: Student Grade Tracker
+Author: Komalpreet Kaur
+Purpose: Simple system to manage student grades
+Date: June 25, 2026
+"""
+
+students = {}
+
+def menu():
+    print("\n--- Student Grade Tracker ---")
+    print("1. Add Student")
+    print("2. Add Grade")
+    print("3. View Students")
+    print("4. Calculate Average")
+    print("5. Exit")
+
+while True:
+    menu()
+    choice = input("Choose option: ")
+
+    if choice == "1":
+        name = input("Student name: ")
+        students[name] = []
+        print("Student added.")
+
+    elif choice == "2":
+        name = input("Student name: ")
+
+        if name in students:
+            grade = float(input("Enter grade: "))
+            students[name].append(grade)
+            print("Grade added.")
+        else:
+            print("Student not found.")
+
+    elif choice == "3":
+        print(students)
+
+    elif choice == "4":
+        name = input("Student name: ")
+
+        if name in students and len(students[name]) > 0:
+            avg = sum(students[name]) / len(students[name])
+            print("Average:", round(avg, 2))
+        else:
+            print("No grades found.")
+
+    elif choice == "5":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid option.")
